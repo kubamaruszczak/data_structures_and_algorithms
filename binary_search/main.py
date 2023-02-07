fa6722c7444b3931ab11ebbf6d990670149e3790
kubamaruszczak <kubamaruszczak@gmail.com>
2023-02-07 17:25:25 +0100

    recursive binary search study

--- binary_search/main.py
+++ binary_search/main.py
@@ -15,6 +15,22 @@ def binary_search(values, target):
     return None
 
 
+def recursive_binary_search(values, target):
+    """Returns Ture if target exist, other way False"""
+    if len(values) == 0:
+        return False
+    else:
+        midpoint = len(values) // 2
+
+        if values[midpoint] == target:
+            return True
+        else:
+            if values[midpoint] < target:
+                return recursive_binary_search(values[midpoint+1:], target)
+            else:
+                return recursive_binary_search(values[:midpoint], target)
+
+
 def verify(index):
     if index is not None:
         print("Target found at index:", index)
@@ -22,8 +38,15 @@ def verify(index):
         print("Target not found in list.")
 
 
+def verify_recursive(result):
+    print("Target found:", result)
+
+
 # Sorted list of numbers
 numbers = [num for num in range(1, 101)]
 
-result = binary_search(numbers, 100)
-verify(result)
+# result = binary_search(numbers, 100)
+# verify(result)
+
+result = recursive_binary_search(numbers, 100)
+verify_recursive(result)
